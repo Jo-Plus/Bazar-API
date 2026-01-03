e4cece8a10f5de1fd69629a9440b5e963cad6276
Fady Alamir <fadyalamer72@gmail.com>
2026-01-03 23:26:52 +0200

    fix: serverless configuration for vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -2,35 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
+const express = require('express');
 const server = express();
-let isInitialized = false;
 
 async function bootstrap() {
-  if (isInitialized) return;
-
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(server),
   );
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
 
   await app.init();
-  isInitialized = true;
 }
 
-const handler = async (req: any, res: any) => {
-  await bootstrap();
-  server(req, res);
-};
+bootstrap();
 
-export default handler;
+module.exports = server;
