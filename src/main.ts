4a99723aa8de8ab5c44fcb78d12069cd460f49ed
Fady Alamir <fadyalamer72@gmail.com>
2026-01-03 23:08:21 +0200

    fix: serverless configuration for vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,9 @@ async function bootstrap() {
   
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
-
   await app.init();
 }
 
 bootstrap();
 
-export default server;
+module.exports = server;
